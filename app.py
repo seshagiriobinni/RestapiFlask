16b5e546f04146988c4d362fc8670bfa58b932cc
Seshagiri Obinni <seshagiri.gvpp@gmail.com>
2022-12-24 22:40:51 +0530

    "Created db.py instead list and changes the code"

--- app.py
+++ app.py
@@ -1,66 +1,55 @@
+import uuid
 from flask import Flask, jsonify
 from flask_restful import request
+from db import stores,items
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My store",
-        "items": [
-            {
-            "name": "Chair",
-            "price": 15.99
-        }
-        ]
-    }
-]
 
 
 @app.get('/stores')
 def get_stores():
-    return jsonify({'Result': stores}, 200)
+    return jsonify({'Result': list(stores.values())}, 200)
 
 
 @app.post('/store')
 def create_store():
-    request_data = request.get_json()
-    print(request_data["name"])
-    store = {"name": request_data["name"], "items": []}
-    stores.append(store)
+    store_data = request.get_json()
+    store_id= uuid.uuid4().hex
+    store = {**store_data,"id":store_id}
+    stores[store_id]=store
     return jsonify({'Result': store, 'message': 'store created successfully'}, 201)
 
-@app.post('/store/<string:name>/item')
-def create_item_in_store(name):
-    request_data=request.get_json()
-    for store in stores:
-        print(name)
-        if store['name'] == name:
-            new_item={"name":request_data["name"],"price":request_data["price"]}
-            store["items"].append(new_item)
-            return jsonify({"new_item":new_item},201)
+@app.post('/item')
+def create_item_in_store():
+    item_data=request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message":"Store not found"},404
+    item_id=uuid.uuid4().hex
+    item={**item_data,"id":item_id}
+    items[item_id]=item
 
-    return jsonify({'Message': "store not found to create item"}, 404)
+    return jsonify({'Message': "Item Created Successfully"}, 201)
+
+@app.get('item')
+def get_all_items():
+    return jsonify({"items":list(items.values())})
+
+@app.get('/store/<string:store_id>')
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return jsonify({'Message': "store_id not found"}, 404)
 
 
-@app.get('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        print(name)
-        if store['name'] == name:
-            return store
 
-    return jsonify({'Message': "store not found"}, 404)
-
-
-
-@app.get('/store/<string:name>/item')
-def get_item_in_store(name):
-    for store in stores:
-        print(name)
-        if store['name'] == name:
-            return jsonify({"items":store["items"]},200)
-
-    return jsonify({'Message': "item not found"}, 404)
+@app.get('/item/<string:item_id>')
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except:
+        return jsonify({'Message': "item not found"}, 404)
 
 
 if __name__ == '__main__':
